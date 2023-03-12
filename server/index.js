a21a154f425b49a367d6f007b259746219218d24
Ivan <ivansavichev884@gmail.com>
2023-03-12 18:39:23 +0300

    FIX: server fixes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const fs = require('fs')
 const http = require('http')
 const WebSocket = require('ws')
+const path = require('path')
 
 const app = express()
+
+app.use(express.static(path.resolve(__dirname, '..', 'dist')))
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
+})
 
 const server = http.createServer(app)
 
@@ -14,7 +21,19 @@ const dispatchEvent = (message, ws) => {
   console.log(json.payload)
   switch (json.event) {
     case 'chat-message':
-      webSocketServer.clients.forEach((client) => client.send(message))
+      const messages =
+        JSON.parse(fs.readFileSync('./messages.json')).messages || []
+      messages.push(json.payload)
+      webSocketServer.clients.forEach((client) =>
+        client.send(
+          JSON.stringify({ event: 'chat-message', payload: messages })
+        )
+      )
+      fs.writeFile(
+        './messages.json',
+        JSON.stringify({ messages }),
+        (err) => console.log
+      )
       break
     default:
       ws.send(new Error('wrong query').message)
@@ -23,14 +42,13 @@ const dispatchEvent = (message, ws) => {
 }
 
 webSocketServer.on('connection', (ws) => {
-  ws.on('message', (m, isBinary) => {
-    console.log(isBinary)
+  ws.on('message', (m) => {
     dispatchEvent(m, ws)
   })
 
   ws.on('error', (e) => ws.send(e))
-
-  ws.send('Hello world!')
+  const messages = JSON.parse(fs.readFileSync('./messages.json')).messages || []
+  ws.send(JSON.stringify({ event: 'chat-message', payload: messages }))
 })
 
 const PORT = process.env.NODE_PORT || 5000
